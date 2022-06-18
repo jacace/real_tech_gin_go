9842e24b5804747b6c2ab455f72db7cf0cdd2db3
jacace <javierandrescaceres@gmail.com>
2022-06-19 00:37:56 +0100

    added lead time calculation

--- pull_request.go
+++ pull_request.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/google/go-github/v45/github"
 
@@ -9,42 +10,54 @@ import (
 )
 
 type PullRequest struct {
-	user string
-	size int
-	id   string
+	leadTimeHrs float64 //date and time for each pull request when opened, and then, when it’s merged
+	timeToMerge float64 //timestamp of the oldest commit of a branch minus the timestamp of the merge commit
+	size        int     //average of total lines of code added plus the total lines of code removed
+	discussions int     //number of comments and reactions per pull request
 }
 
-func NewPullRequest() *PullRequest {
-	return &PullRequest{
-		user: "",
-		id:   "",
-		size: 0,
+type GitHubAPI struct {
+	token string
+}
+
+func NewGitHubAPI(token string) *GitHubAPI {
+	return &GitHubAPI{
+		token: token,
 	}
 }
 
-func (pr *PullRequest) GetSize(personalAccessToken string) (int, error) {
+func (pr *GitHubAPI) GetSize(user string, repo string) (*PullRequest, error) {
 
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: personalAccessToken},
+		&oauth2.Token{AccessToken: pr.token},
 	)
 
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
 	ops := &github.PullRequestListOptions{
-		State:     "Closed",
+		State:     "open", //closed
 		Base:      "main",
 		Sort:      "created",
 		Direction: "asc",
 	}
 
-	prs, _, err := client.PullRequests.List(ctx, "jacace", "real_tech_gin_go", ops)
+	prs, _, err := client.PullRequests.List(ctx, user, repo, ops)
 
 	if err == nil {
-		pr.size = len(prs)
-		return pr.size, nil
+		for _, a := range prs {
+			//a.GetClosedAt()
+			urls := a.GetCommitsURL()
+			fmt.Println("Commits URL:" + urls)
+
+			pr := &PullRequest{
+				leadTimeHrs: (a.GetMergedAt().Sub(a.GetCreatedAt())).Hours(),
+			}
+
+			return pr, nil
+		}
 	}
 
-	return 0, err
+	return nil, err
 }
--- pull_request_test.go
+++ pull_request_test.go
@@ -3,14 +3,14 @@ package main
 import "testing"
 
 func TestGetSize(t *testing.T) {
-	pr := NewPullRequest()
-	size, err := pr.GetSize("")
+	client := NewGitHubAPI("")
+	pr, err := client.GetSize("", "")
 
 	if err != nil {
 		t.Fatal("Error fecthing PR size")
 	}
 
-	if size == 0 {
+	if pr.size == 0 {
 		t.Fatal("Incorrect PR size")
 	}
 }
